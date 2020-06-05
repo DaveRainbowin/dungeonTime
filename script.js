ddfbcbf2b1cb01d83959baa67bf902239b73362b
DaveRainbowin <51142390+DaveRainbowin@users.noreply.github.com>
2020-06-04 20:16:05 -0500

    timer + timer buttons

--- script.js
+++ script.js
@@ -1,11 +1,60 @@
 var totalHP = 50;
 var currentHP = 50;
 var pctHP = currentHP/totalHP * 100;
+var secLeft = 300;
+var minLeft = 5;
+var timerTemp = `5:00`;
 setInterval(update, 100);
+var speed = 1000;
+var timing = setInterval(null, 1000);
+var timerRunning = false;
+var timerFinished = false;
 function update() {
   pctHP = currentHP/totalHP * 100;
   get("hp").style.width = pctHP + "%";
-  get("hpText").innerHTML = currentHP + "/" + totalHP + "HP";
+  get("hpText").innerHTML = `${currentHP}/${totalHP}HP`;
+  if (secLeft == 0) {
+    stopTimer();
+  }
+}
+function startTimer() {
+  if (!timerRunning) {
+    timing = setInterval(timer, speed);
+    timerRunning = true;
+    timerFinished = false;
+  }
+}
+function stopTimer() {
+  clearInterval(timing);
+  timerRunning = false;
+  secLeft = 0;
+  minLeft = 0;
+  timerTemp = `0:00`;
+  timerFinished = true;
+  get("timer").innerHTML = timerTemp;
+}
+function timer() {
+  if (timerRunning) {
+    secLeft--;
+    minLeft = Math.floor(secLeft / 60);
+    if (secLeft % 60 == 0 && secLeft != 0) {
+      timerTemp = `${minLeft}:00`;
+    } else if (secLeft - (60 * minLeft) < 10 & secLeft - (60 * minLeft) > 0) {
+      timerTemp = `${minLeft}:0${secLeft - (60 * minLeft)}`;
+    } else {
+      timerTemp = `${minLeft}:${secLeft - (60 * minLeft)}`;
+    }
+    get("timer").innerHTML = timerTemp;
+  } else if (timerFinished) {
+    timerTemp = `0:00`;
+    get("timer").innerHTML = timerTemp;
+  }
+}
+function accelerate() {
+  if (speed > 500) {
+    speed -= 100;
+    timing = setInterval(timer, speed);
+  }
 }
 function get(id) {
   return document.getElementById(id);
